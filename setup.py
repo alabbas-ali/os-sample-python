74af7bf1b05c3296d274ee046ff648a5561aa4ac
Alabbas <weeamha@gmail.com>
2019-09-25 10:43:12 +0200

    Hello World App

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='HelloWorld',
+      version='1.0',
+      description='Simple flask openshift application',
+      author='Tech Monger',
+      author_email='techmonger@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Flask=0.11.1'],
+     )
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,17 @@
-from flask import Flask
-application = Flask(__name__)
+#!/usr/bin/python
+import os
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
 
-if __name__ == "__main__":
-    application.run()
+# Importing Hello World application
+
+from web_app import app as application
