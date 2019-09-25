c9c310bef609674bf8a0623135e96a01b729aca5
Alabbas <weeamha@gmail.com>
2019-09-25 10:52:18 +0200

    test the

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+#!/usr/bin/python
+import os
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
 
-if __name__ == "__main__":
-    app.run()
+# Importing Hello World application
+
+from test import app as application
